e7ae668bac36a43cd86d2553e0567606dc071607
Tom Boutell <tom@punkave.com>
2018-05-08 15:49:08 -0400

    you can create orderings, and you can put one into effect for a pieces-page. Good progress

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moogBundle: {
+    modules: [ 'apostrophe-pieces-orderings', 'apostrophe-pieces-orderings-pieces', 'apostrophe-pieces-orderings-pieces-pages' ],
+    // 'apostrophe-pieces-orderings-pieces-widgets', 'apostrophe-pieces-orderings-pieces-pages' ],
+    directory: 'lib/modules'
+  }
+};
