579eb4b91da407d06f67c866a8c99a70cfcd2e90
Tom Boutell <tom@punkave.com>
2018-05-09 18:04:27 -0400

    aaaand widgets. We are good

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   moogBundle: {
-    modules: [ 'apostrophe-pieces-orderings', 'apostrophe-pieces-orderings-pieces', 'apostrophe-pieces-orderings-pieces-pages' ],
+    modules: [ 'apostrophe-pieces-orderings', 'apostrophe-pieces-orderings-pieces', 'apostrophe-pieces-orderings-pieces-pages', 'apostrophe-pieces-orderings-pieces-widgets' ],
     // 'apostrophe-pieces-orderings-pieces-widgets', 'apostrophe-pieces-orderings-pieces-pages' ],
     directory: 'lib/modules'
   }
